52a0e80584599ac5b0a22fcc31a1ae3506b6ee7a
hasanbasricaglayan <caglihas@gmail.com>
2025-11-08 01:05:08 +0100

    goroutine: Refactoring

--- goroutine/main.go
+++ goroutine/main.go
@@ -7,16 +7,23 @@ import (
 	"sync"
 )
 
-func backgroundTask(id int, wg *sync.WaitGroup) {
+const (
+	TASK_COUNT = 20
+	URL        = "https://jsonplaceholder.typicode.com/posts"
+)
+
+func task(wg *sync.WaitGroup, id int) {
 	// 3. Tell WaitGroup this task is done = Decrease WaitGroup counter when done
 	defer wg.Done()
 
-	url := "https://jsonplaceholder.typicode.com/posts"
-	resp, err := http.Get(url)
+	resp, err := http.Get(URL)
 	if err != nil {
 		log.Printf("Task %d failed: %v\n", id, err)
 		return
 	}
+
+	// Close the response body to avoid memory leak
+	defer resp.Body.Close()
 
 	fmt.Printf("Task %d, Response Status: %s\n", id, resp.Status)
 }
@@ -25,18 +32,17 @@ func main() {
 	// 1. Create a WaitGroup = Used to wait for all goroutines to finish
 	var wg sync.WaitGroup
 
-	// 2. We are launching 20 goroutines
-	totalTasks := 20
-	for i := 1; i <= totalTasks; i++ {
+	// 2. We are launching TASK_COUNT goroutines
+	for i := 1; i <= TASK_COUNT; i++ {
 		// Increase WaitGroup counter
 		wg.Add(1)
 
-		go backgroundTask(i, &wg)
+		go task(&wg, i)
 	}
 
 	// 4. Wait for all tasks to finish
 	wg.Wait()
-	fmt.Println("All background tasks completed")
+	fmt.Println("All tasks completed")
 
 	fmt.Println("Main function done")
 }
